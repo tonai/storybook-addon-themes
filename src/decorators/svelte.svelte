af25b9b73542730d166fa4004434e54a5b91f312
Tony CABAYE <tony.cabaye@smile.fr>
2020-03-23 17:37:04 +0100

    Fix svelte decorator after merge.

--- src/decorators/svelte.svelte
+++ src/decorators/svelte.svelte
@@ -3,25 +3,26 @@
   import addons from '@storybook/addons';
 
   import { CHANGE, THEME } from '../constants';
-  import { getSelectedTheme } from '../shared';
+  import { getSelectedTheme, getSelectedThemeName } from '../shared';
 
-  import { getHtmlClasses, getTheme } from './shared';
+  import { getHtmlClasses } from './shared';
 
   export let config;
 
   const channel = addons.getChannel();
   const lastValue = channel.last(CHANGE);
+  const { list } = config;
 
-  let theme = (lastValue && lastValue[0]) || getSelectedTheme(config.list);
+  let themeName = (lastValue && lastValue[0]) || getSelectedThemeName(list);
 
-  function setTheme(themeName) {
-    theme = themeName;
+  function setThemeName(theme) {
+    themeName = theme;
   }
 
-  onMount(() => channel.on(CHANGE, setTheme));
-  onDestroy(() => channel.removeListener(CHANGE, setTheme));
+  onMount(() => channel.on(CHANGE, setThemeName));
+  onDestroy(() => channel.removeListener(CHANGE, setThemeName));
 </script>
 
-<div class={getHtmlClasses(getTheme(config, theme))}>
+<div class={getHtmlClasses(getSelectedTheme(list, themeName))}>
   <slot></slot>
 </div>
